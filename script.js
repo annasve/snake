0ea9144e40970da399d4abeaf654745f853a72e1
annasve <svejdovaam@gmail.com>
2024-01-30 20:14:27 +0100

    improved keyboard controls, the snake moves on its own

--- script.js
+++ script.js
@@ -13,20 +13,38 @@ let snakeSpeed = 5;
 let snakePosX = 0;
 let snakePosY = canvas.height / 2 - snakeSize / 2;
 
+//where on each axis is the snake now?
+// // 0,1, -1 >> 0 is snake "stopped", 1 is moves forward, -1 is move backward
+let velocityX = 1;
+let velocityY = 0;
+
 ///////////////////
 //snake move loop
 function gameLoop() {
-  //moveStuff();
+  moveStuff();
   drawStuff();
-  requestAnimationFrame(gameLoop);
+  setTimeout(gameLoop, 1000 / 15); //framerate 15 frames per second
 }
 gameLoop();
 
 function moveStuff() {
   //the snake moves
-  snakePosX += snakeSpeed;
+  snakePosX += snakeSpeed * velocityX;
+  snakePosY += snakeSpeed * velocityY;
+
+  //rules for moving through the walls (entire snake fix with minus)
   if (snakePosX > canvas.width) {
     snakePosX = 0;
+  }
+  if (snakePosX < -snakeSize) {
+    snakePosX = canvas.width;
+  }
+
+  if (snakePosY > canvas.height) {
+    snakePosY = 0;
+  }
+  if (snakePosY < -snakeSize) {
+    snakePosY = canvas.height;
   }
 }
 
@@ -45,35 +63,33 @@ function rectangle(color, x, y, width, height) {
 
 /////////////////////////////////////////////
 
-// //keyboard controls - after pushing a button the snake moves (distance = snakeSpeed)
+// //keyboard controls - improved version - the snake moves on its own, you only give it directions
 function keyPush(evt) {
   switch (evt.key) {
+    //condition: prevent going to the opposite direction (down) right "through" its body
     case 'ArrowLeft':
-      snakePosX -= snakeSpeed;
+      if (velocityX !== 1) {
+        velocityX = -1;
+        velocityY = 0;
+      }
       break;
     case 'ArrowRight':
-      snakePosX += snakeSpeed;
+      if (velocityX !== -1) {
+        velocityX = 1;
+        velocityY = 0;
+      }
       break;
     case 'ArrowUp':
-      snakePosY -= snakeSpeed;
+      if (velocityY !== 1) {
+        velocityX = 0;
+        velocityY = -1;
+      }
       break;
     case 'ArrowDown':
-      snakePosY += snakeSpeed;
+      if (velocityY !== -1) {
+        velocityX = 0;
+        velocityY = 1;
+      }
       break;
-  }
-
-  // rules enabling travelling through the walls - if statements
-  if (snakePosX > canvas.width) {
-    snakePosX = 0;
-  }
-  if (snakePosX < 0) {
-    snakePosX = canvas.width;
-  }
-
-  if (snakePosY > canvas.height) {
-    snakePosY = 0;
-  }
-  if (snakePosY < 0) {
-    snakePosY = canvas.height;
   }
 }
