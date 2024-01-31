d6878a4a89cc53d70b6382c0ab02813dc395eedb
annasve <svejdovaam@gmail.com>
2024-01-31 20:49:44 +0100

    tail and game over

--- script.js
+++ script.js
@@ -11,31 +11,38 @@ console.log(ctx);
 const title = document.querySelector('.score');
 
 let score = 0;
+let gameIsRunning = true;
 
-const fps = 10;
-
+const fps = 15;
 //the snake
-const snakeSize = 50;
-let snakeSpeed = snakeSize;
+const tileSize = 50;
+let snakeSpeed = tileSize;
 let snakePosX = 0;
 let snakePosY = canvas.height / 2;
 
+//direction
 let velocityX = 1;
 let velocityY = 0;
 
 //food
 let foodPosX, foodPosY;
 
+//snake tail
+let tail = [];
+let snakeLength = 5;
+
 //grid tiles
-const tileCountX = canvas.width / snakeSize;
-const tileCountY = canvas.height / snakeSize;
+const tileCountX = canvas.width / tileSize;
+const tileCountY = canvas.height / tileSize;
 
 ///////////////////
 //snake move loop
 function gameLoop() {
-  moveStuff();
-  drawStuff();
-  setTimeout(gameLoop, 1000 / fps); //framerate {fps} frames per second
+  if (gameIsRunning) {
+    drawStuff();
+    moveStuff();
+    setTimeout(gameLoop, 1000 / fps); //framerate {fps} frames per second
+  }
 }
 
 //(run on startup)
@@ -48,23 +55,39 @@ function moveStuff() {
   snakePosY += snakeSpeed * velocityY;
 
   //wall collision
-  if (snakePosX > canvas.width - snakeSize) {
+  if (snakePosX > canvas.width - tileSize) {
     snakePosX = 0;
   }
   if (snakePosX < 0) {
     snakePosX = canvas.width;
   }
-  if (snakePosY > canvas.height - snakeSize) {
+  if (snakePosY > canvas.height - tileSize) {
     snakePosY = 0;
   }
   if (snakePosY < 0) {
     snakePosY = canvas.height;
   }
 
+  //tail collision
+  tail.forEach((snakePart) => {
+    if (snakePart.x === snakePosX && snakePart.y === snakePosY) {
+      gameOver();
+    }
+  });
+
+  //tail
+  tail.push({ x: snakePosX, y: snakePosY });
+
+  //forget earliest parts of the snake
+  tail = tail.slice(-1 * snakeLength);
+
   //food collision
   if (foodPosX === snakePosX && snakePosY === foodPosY) {
     score++;
-    title.textContent = score;
+    title.textContent = `score: ${score}`;
+
+    snakeLength++;
+
     resetFood();
   }
 }
@@ -77,10 +100,15 @@ function drawStuff() {
   drawGrid();
 
   ////snake
-  rectangle('#a0dcc9', snakePosX, snakePosY, snakeSize, snakeSize);
+  rectangle('#a0dcc9', snakePosX, snakePosY, tileSize, tileSize);
 
   ////food
-  rectangle('fuchsia', foodPosX, foodPosY, snakeSize, snakeSize);
+  rectangle('fuchsia', foodPosX, foodPosY, tileSize, tileSize);
+
+  //tail
+  tail.forEach((snakePart) =>
+    rectangle('#a0dcc9', snakePart.x, snakePart.y, tileSize, tileSize),
+  );
 }
 
 function rectangle(color, x, y, width, height) {
@@ -118,6 +146,9 @@ function keyPush(evt) {
         velocityY = 1;
       }
       break;
+    default:
+      if (!gameIsRunning) location.reload();
+      break;
   }
 }
 
@@ -125,20 +156,36 @@ function keyPush(evt) {
 function drawGrid() {
   for (let i = 0; i < tileCountX; i++) {
     for (let j = 0; j < tileCountY; j++) {
-      rectangle(
-        '#fff',
-        snakeSize * i,
-        snakeSize * j,
-        snakeSize - 1,
-        snakeSize - 1,
-      );
+      rectangle('#fff', tileSize * i, tileSize * j, tileSize - 1, tileSize - 1);
     }
   }
 }
 
 //random food position
 function resetFood() {
-  foodPosX = Math.floor(Math.random() * tileCountX) * snakeSize;
-  foodPosY = Math.floor(Math.random() * tileCountY) * snakeSize;
-  //snakeSize - represents tile width
+  foodPosX = Math.floor(Math.random() * tileCountX) * tileSize;
+  foodPosY = Math.floor(Math.random() * tileCountY) * tileSize;
+  //tileSize - represents tile width
+
+  //don't spawn food on snake's head
+  if (foodPosX === snakePosX && foodPosY === snakePosY) resetFood();
+
+  //don't spawn food on the tail
+  if (
+    tail.some(
+      (snakePart) => snakePart.x === foodPosX && snakePart.y === foodPosY,
+    )
+  ) {
+    resetFood();
+  }
+
+  //when snake fills all tiles
+  if (snakeLength === tileCountX * tileCountY) {
+    gameOver();
+  }
+}
+
+function gameOver() {
+  gameIsRunning = false;
+  title.style.fontSize = '4rem';
 }
